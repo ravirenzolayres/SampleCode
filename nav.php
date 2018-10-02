f06c0134b56cc8be3ebc39d50baab2d682db06ff
Ravi Renz Olayres <renzolayres88@gmail.com>
2018-10-02 17:56:40 +0800

    Nav & Index update
    Nav & Index update

--- nav.php
+++ nav.php
@@ -10,7 +10,7 @@
 				</button>
 			</div>
 			<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav navbar-right">
+				<ul class="nav navbar-nav navbar-center">
 					<li id="navHome" class="active"><a href="#home">Home</a></li>
 					<li id="navProjects"><a href="#projects">Project</a></li>
 					<li id="navContact"><a href="#contact">Contact</a></li>
